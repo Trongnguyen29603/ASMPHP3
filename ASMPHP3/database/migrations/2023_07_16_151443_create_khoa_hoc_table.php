63b9afa2223c1d0d1d81d74904b8e09fb849957e
Trongnguyen29603 <viettrong29603@gmail.com>
2023-07-16 22:25:05 +0700

    first commit

---
+++ ASMPHP3/database/migrations/2023_07_16_151443_create_khoa_hoc_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('khoa_hoc', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('price');
+            $table->string('describe');
+            $table->string('process');
+            $table->integer('id_category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('khoa_hoc');
+    }
+};
---
+++ ASMPHP3/database/migrations/2023_07_16_151651_create_giao_vien_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('giao_vien', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('phone');
+            $table->string('email');
+            $table->integer('password');
+            $table->string('speciallzi');
+            $table->string('image');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('giao_vien');
+    }
+};
